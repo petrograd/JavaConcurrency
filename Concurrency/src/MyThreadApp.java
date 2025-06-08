5e4fb95f5e4963f03baef3048c89b72b818edbff
Petr <pmerkuryev@gmail.com>
2025-06-08 17:52:34 +0200

    simple MyThread added

---
+++ Concurrency/src/MyThreadApp.java
@@ -0,0 +1,27 @@
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < MyThreadApp.SIZE; i++) {
+            System.out.println("    R = " + i);
+        }
+    }
+}
+public class MyThreadApp {
+    public final static long SIZE = 100;
+
+    public static void main(String[] args) {
+        MyThread thread = new MyThread();
+        thread.start();
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < SIZE; i++) {
+                System.out.println("            Tr2 = " + i);
+            }
+        });
+        thread2.start();
+
+        for (int i = 0; i < SIZE; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
